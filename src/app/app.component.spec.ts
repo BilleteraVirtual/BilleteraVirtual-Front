e901a63bfbbfe03199ce40fba8ae1a9966dbc13c
Juanchi0207 <juanchi.sampieri@gmail.com>
2024-08-19 08:04:27 -0300

    Agregamos nueva estructura ci-cd

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('Deberia tener un Hola mundo', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hola Mundo');
+  });
+});
